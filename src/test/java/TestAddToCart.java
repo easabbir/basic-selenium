64fecc6542347667b87687271c6f473377c7057e
Ehsanul Alam Sabbir <easabbir@gmail.com>
2025-10-03 09:19:08 +0600

    added add to cart test case

--- src/test/java/TestAddToCart.java
+++ src/test/java/TestAddToCart.java
@@ -1,2 +1,24 @@
-public class TestAddToCart {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class TestAddToCart extends BaseClass{
+
+
+
+    @Test
+    public void testAddToCart() throws InterruptedException {
+        BaseClass.setupDriver();
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@class='submit-button btn_action']")).click();
+        driver.findElement(By.xpath("//input[@class='submit-button btn_action']")).click();
+        driver.findElement(By.xpath("//input[@class='submit-button btn_action']")).click();
+        driver.findElement(By.xpath("//input[@class='submit-button btn_action']")).click();
+
+
+
+    }
 }
